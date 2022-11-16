e2269515fae6a77716148f7432b47daa6be67aec
deoj <deoj.phil@yahoo.ca>
2022-11-15 20:47:28 -0700

    added content

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,11 @@ module.exports = {
   darkMode: 'media', // or 'class'
   theme: {
     screens: {
+      xs: '425px',
       sm: '640px',
       md: '768px',
       lg: '1024px',
       xl: '1280px',
-      '2xl': '1536px',
     },
     supports: {},
     colors: ({ colors }) => ({
